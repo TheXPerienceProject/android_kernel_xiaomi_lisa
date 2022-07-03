2a81f50d935658ac5e03af1a32a1d1b95bba9405
klozz <carlosj@klozz.dev>
2022-07-03 10:49:38 +0200

    task_mmu: abort reclaim once process goes to foreground
    App compaction can not be aborted due to current design,
    it causes app get stuck for a while when it resume to foreground
    if app is compacting by kernel. So now we add check in kernel
    while loop to abort the compaction if adj is zero.
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1967,6 +1967,9 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 	if (type == RECLAIM_RANGE) {
 		vma = find_vma(mm, start);
 		while (vma) {
+			/*  abort reclaim if app goes to foreground. */
+			if (task->signal->oom_score_adj == 0)
+				break;
 			if (vma->vm_start > end)
 				break;
 			if (is_vm_hugetlb_page(vma))
@@ -1979,6 +1982,10 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 		}
 	} else {
 		for (vma = mm->mmap; vma; vma = vma->vm_next) {
+			/* abort reclaim if app goes to foreground. */
+			if (task->signal->oom_score_adj == 0)
+				break;
+
 			if (is_vm_hugetlb_page(vma))
 				continue;
 
